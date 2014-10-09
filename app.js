6cacf4157982d671123c4fae2857d128c14cbeef
Fredrik Lind <fredrik@editerat.se>
2014-10-09 19:59:10 +0200

    Changed to port 80

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ server.get('/echo/:name', function(req, res, next) {
     return next();
 });
 
-server.listen(8080, function() {
+server.listen(80, function() {
     console.log('%s listening at %s', server.name, server.url);
 });
 
