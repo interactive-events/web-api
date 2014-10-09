43dc322cb110833d5867a82ff7630d54c2d5b3c7
Fredrik Lind <fredrik@editerat.se>
2014-10-09 16:10:39 +0200

    Deploy added

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 var restify = require('restify');
 
 var server = restify.createServer({
-  name: 'myapp',
-  version: '1.0.0'
+    name: 'myapp',
+    version: '1.0.0'
 });
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
-server.get('/echo/:name', function (req, res, next) {
-  res.send(req.params);
-  return next();
+server.get('/', function(req, res, next) {
+    res.send(listAllRoutes(server));
+    return next();
 });
 
-server.listen(8080, function () {
-  console.log('%s listening at %s', server.name, server.url);
+server.get('/echo/:name', function(req, res, next) {
+    res.send(req.params);
+    return next();
 });
+
+server.listen(8080, function() {
+    console.log('%s listening at %s', server.name, server.url);
+});
+
+function listAllRoutes(server) {
+    var response = {};
+
+    // GET routes
+    server.router.routes.GET.forEach(
+        function(value) {
+            response.GET = value.spec.path;
+        }
+    );
+    return response;
+}
