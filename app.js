7351701533f9623dc70f42319e8262ba00422b20
Fredrik Lind <fredrik@editerat.se>
2014-10-10 21:34:14 +0200

    Restructured project, added Mocha for testing

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
-var restify = require('restify');
+exports.StartServer = function() {
+    var restify = require('restify');
 
-var server = restify.createServer({
-    name: 'myapp',
-    version: '1.0.0'
-});
-server.use(restify.acceptParser(server.acceptable));
-server.use(restify.queryParser());
-server.use(restify.bodyParser());
+    var server = restify.createServer({
+        name: 'Interactive Events API',
+        version: '1.0.0'
+    });
+    server.use(restify.acceptParser(server.acceptable));
+    server.use(restify.queryParser());
+    server.use(restify.bodyParser());
 
-server.get('/', function(req, res, next) {
-    res.send(listAllRoutes(server));
-    return next();
-});
+    server.get('/', function(req, res, next) {
+        res.send(listAllRoutes(server));
+        return next();
+    });
 
-server.get('/echo/:name', function(req, res, next) {
-    res.send(req.params);
-    return next();
-});
+    server.get('/echo/:name', function(req, res, next) {
+        res.send(req.params);
+        return next();
+    });
 
-server.listen(80, function() {
-    console.log('%s listening at %s', server.name, server.url);
-});
+    server.listen(8081, function() {
+        console.log('%s listening at %s', server.name, server.url);
+    });
 
-function listAllRoutes(server) {
-    var response = {};
+    function listAllRoutes(server) {
+        var response = {};
 
-    // GET routes
-    server.router.routes.GET.forEach(
-        function(value) {
-            response.GET = value.spec.path;
-        }
-    );
-    return response;
+        // GET routes
+        server.router.routes.GET.forEach(
+            function(value) {
+                response.GET = value.spec.path;
+            }
+        );
+        return response;
+    }
 }
---
+++ test/interactive-events-api_test.js
@@ -0,0 +1,47 @@
+/*global describe,it,before*/
+'use strict';
+
+var assert = require('assert');
+var restify = require('restify');
+
+// init the test client
+var client = restify.createJsonClient({
+	url: 'http://127.0.0.1:8081',
+	version: '*'
+});
+
+// Container for all tests
+describe('API tests', function() {
+	before(function(done) {
+		require('../app').StartServer();
+		done();
+	});
+
+	// Container for GET tests 
+	describe('GET: /echo/test', function() {
+		// Test #1
+		describe('200 response check', function() {
+			it('should get a 200 response', function(done) {
+				client.get('/echo/test', function(err, req, res, data) {
+					assert.ifError(err);
+					if (res.statusCode !== 200) {
+						throw new Error('invalid response from /echo/test');
+					}
+					done();
+				});
+			});
+		});
+
+		// Test #2
+		describe('response fields', function() {
+			it('should get contain name field with a value of test', function(done) {
+				client.get('/echo/test', function(err, req, res, data) {
+					if (data.name !== "test") {
+						throw new Error('invalid response, expected test as name parameter in response');
+					}
+					done();
+				});
+			});
+		});
+	});
+});
