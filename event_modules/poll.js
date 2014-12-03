dc023777a5a450baf82b277ced6eb8655b6fc535
Sofie Nilsson <sofnii-9@student.ltu.se>
2014-12-03 11:11:10 +0100

    Restructure of GET events/id/activity/id i poll.js

--- event_modules/poll.js
+++ event_modules/poll.js
@@ -18,49 +18,6 @@ function isNamespaceActive(socketNameSpace) {
 }
 
 module.exports = function(server) {
-/*
-	server.get('/events/:eventId/activities/:activityId', authenticate, function(req, res, next) {
-		// [] is user admin for this event - isAdmin = true
-
-		// has user already voted in this poll?
-		var hasVoted = null;
-		db.PollVoter.findOne({ 'activity': req.params.activityId, 'userId': req.user._id }).exec(function (err, pollVoter) {
-			if (err) return res.send(404);
-			if (pollVoter != null) {
-				console.log(pollVoter);
-				hasVoted = true;
-			} else {
-				hasVoted = false;
-			}
-			//console.log("hasVoted=", hasVoted);
-		});
-
-		// is the poll still active (eg. is the event still ongoing?)
-		var eventFinished = null;
-		db.Event.findById(req.params.eventId).lean().exec(function (err, events) {
-			if(err) return res.send(404);
-			var currentTime = new Date();
-			var eventEndTime = new Date(events.time.end);
-			if (currentTime.getTime() > eventEndTime.getTime()) {
-				eventFinished = true;
-			} else {
-				eventFinished = false;
-			}
-			//console.log("eventFinished=", eventFinished);
-		});
-
-		db.Activity.findById(req.params.activityId).lean().exec(function (err, activity) {
-			if (err) return res.send(404);
-			//console.log("activity (before)=",activity);
-			activity["id"] = activity["_id"];
-			delete activity["_id"];
-			delete activity["__v"];
-			activity.customData.hasVoted = hasVoted;
-			activity.customData.eventFinished = eventFinished;
-			//console.log("activity (after)=",activity);
-			res.send(activity);
-		});
-	});*/
 
 	server.put('/events/:eventId/activities/:activityId/vote', authenticate, function(req, res, next) {
 
@@ -79,12 +36,52 @@ module.exports = function(server) {
 			}
 		});
 	});
-}
+};
+
 module.exports.get = function(req, res, next, event, activity) {
 	activity.customData.superDuperCustomStuff = "asdasdasd"; // example, remove this
 	activity.eventId = event.id; // example, remove this
-	res.send(activity);
-};
+
+	// has user already voted in this poll?
+	var hasVoted = null;
+	db.PollVoter.findOne({ 'activity': activity.id, 'userId': req.user._id }).exec(function (err, pollVoter) {
+		if (err) return res.send(500, err);
+		if (pollVoter != null) {
+			console.log("pollVoter=", pollVoter);
+			hasVoted = true;
+		} else {
+			hasVoted = false;
+		}
+		//console.log("hasVoted=", hasVoted);
+	});
+
+	// is the poll still active (eg. is the event still ongoing?)
+	var eventFinished = null;
+	db.Event.findById(event.id).lean().exec(function (err, events) {
+		if(err) return res.send(500, err);
+		var currentTime = new Date();
+		var eventEndTime = new Date(events.time.end);
+		if (currentTime.getTime() > eventEndTime.getTime()) {
+			eventFinished = true;
+		} else {
+			eventFinished = false;
+		}
+		//console.log("eventFinished=", eventFinished);
+	});
+
+	db.Activity.findById(activity.id).lean().exec(function (err, activity) {
+		if (err) return res.send(500, err);
+		//console.log("activity (before)=",activity);
+		activity["id"] = activity["_id"];
+		delete activity["_id"];
+		delete activity["__v"];
+		activity.customData.hasVoted = hasVoted;
+		activity.customData.eventFinished = eventFinished;
+		//console.log("activity (after)=",activity);
+		res.send(activity);
+	});
+}
+
 module.exports.start = function(req, res, next, socketNameSpace, activityId) {
 	if(isNamespaceActive(socketNameSpace) == true) {
 		return res.send(410); // 410: Gone
@@ -125,6 +122,4 @@ module.exports.start = function(req, res, next, socketNameSpace, activityId) {
 	}, 0); // fork
 	*/
 	return res.send(201);
-}
-
-
+};
